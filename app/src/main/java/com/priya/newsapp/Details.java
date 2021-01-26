b58fc79827761a08efd1729e25e1832593352be8
priya <priyadharshinicseksrct@gmail.com>
2021-01-26 09:30:40 +0530

    newsapp

---
+++ app/src/main/java/com/priya/newsapp/Details.java
@@ -0,0 +1,67 @@
+package com.priya.newsapp;
+
+import androidx.appcompat.R;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+public class Details extends AppCompatActivity {
+
+    TextView Title, Source, Time, Desc;
+    ImageView imageView;
+    WebView webView;
+    ProgressBar loader;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.details);
+
+        Title = findViewById(R.id.T1);
+        Source = findViewById(R.id.S1);
+        Time = findViewById(R.id.D1);
+        Desc = findViewById(R.id.tvDesc);
+
+        imageView = findViewById(R.id.imageView);
+
+        webView = findViewById(R.id.webView);
+
+        loader = findViewById(R.id.webViewLoader);
+        loader.setVisibility(View.INVISIBLE);
+
+        Intent intent = getIntent();
+        String title = intent.getStringExtra("title");
+        String source = intent.getStringExtra("source");
+        String time = intent.getStringExtra("time");
+        String desc = intent.getStringExtra("desc");
+        String imageUrl = intent.getStringExtra("imageUrl");
+        String url = intent.getStringExtra("url");
+
+
+        Title.setText(title);
+        Source.setText(source);
+        Time.setText(time);
+        Desc.setText(desc);
+
+        Picasso.with(Details.this).load(imageUrl).into(imageView);
+
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setLoadsImagesAutomatically(true);
+        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl(url);
+        if (webView.isShown()){
+            loader.setVisibility(View.INVISIBLE);
+        }
+    }
+}
---
+++ app/src/main/java/com/priya/newsapp/MainActivity.java
@@ -0,0 +1,143 @@
+package com.priya.newsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.priya.newsapp.parameter.Articles;
+import com.priya.newsapp.parameter.Headlines;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    SwipeRefreshLayout swipeRefreshLayout;
+    EditText editText;
+    Button button, AU;
+    Dialog dialog;
+    final String API_KEY = "c6d002f971d1419584b415f5057d69c4";
+    Adapter adapter;
+    List<Articles>  articles = new ArrayList<>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        swipeRefreshLayout = findViewById(R.id.swipeRefresh);
+        recyclerView = findViewById(R.id.recyclerView);
+
+        editText = findViewById(R.id.ET1);
+        button = findViewById(R.id.Btn1);
+        AU = findViewById(R.id.btn2);
+        dialog = new Dialog(MainActivity.this);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        final String country = getCountry();
+
+
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                retrieveJson("",country,API_KEY);
+            }
+        });
+        retrieveJson("",country,API_KEY);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!editText.getText().toString().equals("")){
+                    swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+                        @Override
+                        public void onRefresh() {
+                            retrieveJson(editText.getText().toString(),country,API_KEY);
+                        }
+                    });
+                    retrieveJson(editText.getText().toString(),country,API_KEY);
+                }else{
+                    swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+                        @Override
+                        public void onRefresh() {
+                            retrieveJson("",country,API_KEY);
+                        }
+                    });
+                    retrieveJson("",country,API_KEY);
+                }
+            }
+        });
+
+        AU.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog();
+            }
+        });
+
+    }
+
+    public void retrieveJson(String query ,String country, String apiKey){
+
+
+        swipeRefreshLayout.setRefreshing(true);
+        Call<Headlines> call;
+        if (!editText.getText().toString().equals("")){
+            call= ApiClient.getInstance().getApi().getSpecificData(query,apiKey);
+        }else{
+            call= ApiClient.getInstance().getApi().getHeadlines(country,apiKey);
+        }
+
+        call.enqueue(new Callback<Headlines>() {
+            @Override
+            public void onResponse(Call<Headlines> call, Response<Headlines> response) {
+                if (response.isSuccessful() && response.body().getArticles() != null){
+                    swipeRefreshLayout.setRefreshing(false);
+                    articles.clear();
+                    articles = response.body().getArticles();
+                    adapter = new Adapter(MainActivity.this,articles);
+                    recyclerView.setAdapter(adapter);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Headlines> call, Throwable t) {
+                swipeRefreshLayout.setRefreshing(false);
+                Toast.makeText(MainActivity.this, t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public String getCountry(){
+        Locale locale = Locale.getDefault();
+        String country = locale.getCountry();
+        return country.toLowerCase();
+    }
+
+    public void showDialog(){
+        Button btnClose;
+        dialog.setContentView(R.layout.test);
+        dialog.show();
+        btnClose = dialog.findViewById(R.id.btn3);
+
+        btnClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+    }
+}
